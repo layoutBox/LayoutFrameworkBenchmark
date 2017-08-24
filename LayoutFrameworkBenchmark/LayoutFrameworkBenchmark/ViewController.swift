9a2a271099cc29e991378dc6a54091f4b0e2deb6
Luc Dion <ldion@mirego.com>
2017-08-24 08:42:38 -0400

    initial

---
+++ LayoutFrameworkBenchmark/LayoutFrameworkBenchmark/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LayoutFrameworkBenchmark
+//
+//  Created by DION, Luc (MTL) on 2017-08-24.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+use_frameworks!
+
+project 'LayoutFrameworkBenchmark.xcodeproj'
+
+platform :ios, '10.0'
+
+inhibit_all_warnings!
+
+target 'LayoutFrameworkBenchmark' do
+    pod 'PinLayout'
+    pod 'FlexLayout'
+    pod 'Reveal-SDK'
+end
