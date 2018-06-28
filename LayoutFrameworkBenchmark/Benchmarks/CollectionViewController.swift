3b86422e0237b98c43b4547c9ccd8f1ec04338f4
Luc Dion <ldion@mirego.com>
2018-06-28 09:22:15 -0400

    Update PinLayout to version 1.7.9

--- LayoutFrameworkBenchmark/Benchmarks/CollectionViewController.swift
+++ LayoutFrameworkBenchmark/Benchmarks/CollectionViewController.swift
@@ -62,7 +62,7 @@ class CollectionViewController<ContentViewType: UIView>: UICollectionViewControl
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell: CellType = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CellType
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CellType
         cell.setData(data[indexPath.row])
         return cell
     }
